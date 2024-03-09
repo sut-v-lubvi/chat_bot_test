b759321d43bebb5598fc4691a679c3ed904c1cfb
Alexander Gladyshev <ASGladyshev@inno.tech>
2024-03-09 14:03:45 +0300

    install axios and cheerio

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 const token = '6755071690:AAFYYRrQJc1o_NPNUKQavOuc9ILSl428f5U';
 
@@ -12,9 +14,24 @@ bot.onText(/\/start/, (msg) => {
 
 });
 
-bot.on('message', (msg) => {
-    console.log(msg)
+bot.onText(/\/getdata/, async (msg) => {
     const chatId = msg.chat.id;
-    const text = msg.text
-    // bot.sendMessage(chatId, 'Здорова чушка!');
+
+    const url = 'https://dzen.ru/a/X-hGvw0Md1msvlZB';
+    try {
+        const response = await axios.get(url);
+        console.log(response)
+        const $ = cheerio.load(response.data);
+        const data = $('.publication-columns-layout__columns-container').text();
+        console.log(`Данные с горнолыжного курорта: ${data}`)
+
+        bot.sendMessage(chatId, `Данные с горнолыжного курорта: ${data}`);
+    } catch (error) {
+        console.error('Ошибка при получении данных:', error);
+        bot.sendMessage(chatId, 'Произошла ошибка при получении данных с сайта горнолыжного курорта.');
+    }
 });
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Отправьте мне команду /getdata, чтобы получить данные с горнолыжного курорта.');});
